d8a50025a5777701091e2feebcd4df461ca06ebd
ilvecho <massimoterzi@hotmail.it>
2025-02-20 23:01:31 +0100

    added new parsings, now we are almost done

--- src/utils/parse_emails.py
+++ src/utils/parse_emails.py
@@ -48,11 +48,14 @@ class ParseEmails:
         elif from_address == "joe@readthejoe.com":
             content = self.average_joe(content)
 
+        elif from_address == "theneuron@newsletter.theneurondaily.com":
+            content = self.neuron(content)
+        
         elif from_address == "news@myanalitica.it":
             content = self.analitica(content)
 
-        elif from_address == "theneuron@newsletter.theneurondaily.com":
-            content = self.neuron(content)
+        elif from_address == "community@datapizza.it":
+            content = self.datapizza(content)
 
         return content
     
@@ -170,6 +173,63 @@ class ParseEmails:
 
         return content.strip()
 
+    # The Neuron
+    def neuron(self, content):
+
+        content = content.split("Welcome, humans. ")[-1].strip()
+        content = content.split("# A Cat's Commentary.")[0].strip()
+
+        # Remove the links
+        pattern = r'\(http.*?\)'
+        content = re.sub(pattern, '', content)
+        pattern = r'\(s:.*?\)'
+        content = re.sub(pattern, '', content)
+        # pattern = r'(//.*?)'
+        # content = re.sub(pattern, '', content)
+        pattern = r'\(www.*?/\)'
+        content = re.sub(pattern, '', content)
+        # pattern = r'(.*?/)'
+        # content = re.sub(pattern, '', content)
+
+        # Remove the images
+        pattern = r'View image:.*?Caption:.*?\n'
+        content = re.sub(pattern, '', content, flags=re.DOTALL)
+
+        # Remove the partnerships
+        pattern = r'###### \*\*FROM OUR PARTNERS\*\*.*#.*#'
+        content = re.sub(pattern, '#', content, flags=re.DOTALL)
+
+        # Remove Treats to try
+        pattern = r'# Treats To Try\..*# Around the Horn'
+        content = re.sub(pattern, '# Around the Horn', content, flags=re.DOTALL)
+
+        # Remove the daily special, as it is always based on images
+        if '# Monday' in content:
+            content = content.split("# Monday")[0].strip()
+        elif '# Tuesday' in content:
+            content = content.split("# Tuesday")[0].strip()
+        elif '# Wednesday' in content:
+            content = content.split("# Wednesday")[0].strip()
+        elif '# Thursday' in content:
+            content = content.split("# Thursday")[0].strip()
+        elif '# Friday' in content:
+            content = content.split("# Friday")[0].strip()
+
+        # Remove empty lines while keeping paragraph separation
+        paragraphs = content.split('\r\n')
+        cleaned_paragraphs = []
+
+        for paragraph in paragraphs:
+            # Remove newline characters within paragraphs
+            cleaned_paragraph = ' '.join(paragraph.split())
+            cleaned_paragraphs.append(cleaned_paragraph)
+
+        cleaned_paragraphs = [paragraph.strip() if len(paragraph) > 2 else "\n"  for paragraph in cleaned_paragraphs]
+        # Join the cleaned paragraphs with double newlines to keep separation
+        content = ''.join(cleaned_paragraphs)
+
+        return content.strip()
+    
     # Analitica
     def analitica(self, content):
 
@@ -205,27 +265,51 @@ class ParseEmails:
 
         return content.strip()
 
-    # The Neuron
-    def neuron(self, content):
+    # Datapizza
+    def datapizza(self, content):
 
-        content = content.split("Welcome, humans. ")[-1].strip()
-        content = content.split("# A Cat's Commentary.")[0].strip()
+        # content = content.split("Buona lettura!")[-1].strip()
+        content = content.split("Datapizza Selection")[0].strip()
+
+        content = content.replace("Image\r", "").strip()
 
         # Remove the links
-        pattern = r'\(http.*?\)'
+        pattern = r'\( http.*?\)'
         content = re.sub(pattern, '', content)
-        pattern = r'\(s:.*?\)'
-        content = re.sub(pattern, '', content)
-        # pattern = r'(//.*?)'
-        # content = re.sub(pattern, '', content)
-        pattern = r'\(www.*?/\)'
-        content = re.sub(pattern, '', content)
-        # pattern = r'(.*?/)'
-        # content = re.sub(pattern, '', content)
 
-        # Remove the images
-        pattern = r'View image:.*?Caption:.*?\n'
+        # Remove emojis
+        emoji_pattern = re.compile("["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            u"\U00002702-\U000027B0"  # dingbats
+            u"\U000024C2-\U0001F251"  # misc symbols
+            u"\U0000FE0F"  # variation selector
+            u"\U0000231A-\U0000231B"  # watch symbols
+            u"\U000023E9-\U000023EC"  # play/pause symbols
+            u"\U000023F0-\U000023F3"  # clock symbols
+            u"\U000023F8-\U000023FA"  # media symbols
+            u"\U00002300-\U000023FF"  # technical symbols
+            u"\U00002600-\U000027BF"  # misc symbols and arrows
+            u"\U00002934-\U00002935"  # arrow symbols
+            u"\U00002B05-\U00002B07"  # arrow symbols
+            u"\U00002B1B-\U00002B1C"  # square symbols
+            u"\U00002B50-\U00002B55"  # star symbols
+            "]+", flags=re.UNICODE)
+        
+        # Remove emojis
+        content = emoji_pattern.sub('', content)
+
+        # Remove Signature
+        pattern = r"---------------------------.*?By.*?alt=''''"
         content = re.sub(pattern, '', content, flags=re.DOTALL)
+
+        # pattern = r'In collaborazione con.*?News'
+        # content = re.sub(pattern, 'News', content, flags=re.DOTALL)
+
+        # pattern = r'La trimestrale.*?Extra'
+        # content = re.sub(pattern, 'Extra', content, flags=re.DOTALL)
 
         # Remove empty lines while keeping paragraph separation
         paragraphs = content.split('\r\n')
@@ -240,4 +324,13 @@ class ParseEmails:
         # Join the cleaned paragraphs with double newlines to keep separation
         content = ''.join(cleaned_paragraphs)
 
+        content = content.replace("alt=''''", "")
+
+        content = '\n'.join(content.split('\n')[:-2])
+
         return content.strip()
+
+
+
+
+
